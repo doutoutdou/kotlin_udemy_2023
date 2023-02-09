b099782f6864923a3e95a3ec9a7ee5bd231308e4
doudou <edouard.talma@gmail.com>
2023-02-08 22:00:39 -0500

    round 1

---
+++ kotlin_udemy_2023/src/main/kotlin/Challenge1.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+
+    val hello1 = "Hello"
+    val hello2 = "Hello"
+
+    // test de reference
+    println(hello1 === hello2)
+
+    // test de comparaison dobjet
+    println(hello1 == hello2)
+
+    var number = 2988
+
+    val value : Any = "any type"
+    if (value is String) {
+        println(value.uppercase())
+    }
+
+    val ones = """    1   
+                 |   11
+                 |  111
+                 | 1111""".trimMargin()
+
+    // juste pour lexemple
+    println("$ones")
+
+
+}
